6ad95ca6752d5509f023c5f2c09dd985ce758586
Juan Antonio gonzález Vidal <juanantoniogovidal@gmail.com>
2026-01-22 18:10:21 +0100

    ajutes funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       <div className='bg-[#0c1a0a] flex-1 '>
         <div className="flex flex-col mx-25 mt-50 max-w-md text-white">
           <h1 className="uppercase font-bold text-5xl mb-4">Domina tu flujo de <br /><span className="text-green-600">trabajo</span></h1>
-          <p className="text-gray-300 text-2xl">una plataforma todo en uno para administrar tus proyectos, tareas y equipos de manera eficiente</p>
+          <p className="text-gray-300 text-2xl">La herramienta Kanban definitiva para equipos de alto rendimiento. Optimiza tareas, visualiza el progreso y agiliza los envíos.</p>
         </div>
       <div className='absolute bottom-20 left-20 bg-white/10 backdrop-blur-md border border-white/20 rounded-full px-4 py-3 flex items-center gap-3 w-fit hover:bg-white/20 transition-all cursor-pointer'>
         <img src='https://via.placeholder.com/40' alt='jagovi' className='w-10 h-10 rounded-full object-cover' />
@@ -74,9 +74,22 @@ function App() {
             </input>
           </div>
           <div className='relative'> {/* //TODO:  cambiar icono */}
-            <label htmlFor="password" className="block text-sm font-bold">Contraseña</label>
-            <a onClick={() => {handleContraseña(!mostrarContraseña)} } className='absolute right-4 top-9 hover:text-green-600 cursor-pointer'>👁️</a>
-            <input type={mostrarContraseña ? "text" : "password"} key="password" className="w-full h-14 px-4 bg-background-light border border-gray-100 rounded-lg focus:ring-2 focus:ring-green-600/20 focus:border-green-600 outline-none transition-all duration-200 text-[#101d0c] placeholder:text-gray-400" placeholder="********" value={credenciales.password} onChange={(e) => setCredenciales({...credenciales, password: e.target.value})}/>
+            <div className='flex justify-between'>
+              <label htmlFor="password" className="block text-sm font-bold">Contraseña</label>
+              <a className='text-green-600 font-bold hover:underline' href="">¿olvidaste la Contraseña?</a>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer absolute right-4 top-9 hover:text-green-600" onClick={() => {handleContraseña(!mostrarContraseña)} }>
+              {mostrarContraseña ?
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                  : 
+                <>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </>
+              }     
+            </svg>
+
+            <input type={mostrarContraseña ? "text" : "password"} key="password" className="w-full h-14 px-4 bg-background-light border border-gray-100 rounded-lg focus:ring-2 focus:ring-green-600/20 focus:border-green-600 outline-none transition-all duration-200 text-[#101d0c] placeholder:text-gray-400" placeholder={mostrarContraseña ? "Contraseña" : "••••••••••"} value={credenciales.password} onChange={(e) => setCredenciales({...credenciales, password: e.target.value})}/>
           </div>
           <div>
            <button className="uppercase w-full h-14 bg-green-600 text-white font-bold rounded-lg shadow-lg shadow-primary/20 hover:bg-green-600/90 transition-all active:scale-[0.98]" type='submit'>
