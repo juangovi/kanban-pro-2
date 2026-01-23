eb25855c463461f79256b3f441e97a1340aceb03
Juan Antonio gonzález Vidal <juanantoniogovidal@gmail.com>
2026-01-23 13:11:11 +0100

    firebase auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,11 @@
- import { useState } from 'react'
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { db } from './firebaseConfig';
+import { SingIn } from "./pages/SingIn.jsx";
 
 function App() {
-
-  const [credenciales, setCredenciales] = useState({
-    email: '',
-    password: ''
-  })
-
-  const buscarUsuario = async (email, password) => {
-    const q = query(collection(db, "usuarios"), where("email", "==", email),where("password", "==", password));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    });
-  }
-
-  const [mostrarContraseña, setmostrarContraseña] = useState(false);
-
-  const handleContraseña = (valor) => {
-    setmostrarContraseña(valor);
-  }
-
+  return (<>
+      <SingIn></SingIn>
+    </>
+  )
  
-  return (
-    <div className='flex w-full h-screen'>
-      <div className='bg-[#0c1a0a] flex-1 '>
-        <div className="flex flex-col mx-25 mt-50 max-w-md text-white">
-          <h1 className="uppercase font-bold text-5xl mb-4">Domina tu flujo de <br /><span className="text-green-600">trabajo</span></h1>
-          <p className="text-gray-300 text-2xl">La herramienta Kanban definitiva para equipos de alto rendimiento. Optimiza tareas, visualiza el progreso y agiliza los envíos.</p>
-        </div>
-      <div className='absolute bottom-20 left-20 bg-white/10 backdrop-blur-md border border-white/20 rounded-full px-4 py-3 flex items-center gap-3 w-fit hover:bg-white/20 transition-all cursor-pointer'>
-        <img src='https://via.placeholder.com/40' alt='jagovi' className='w-10 h-10 rounded-full object-cover' />
-        <span className='text-white text-sm font-medium'>Creado por jagovi</span>
-      </div>
-      </div>
-      <div className='bg-white flex-1 flex flex-col justify-center items-center'>
-        <div className="mb-8">
-          <h1 className="font-bold text-3xl uppercase">bienvenido de vuelta</h1>
-          <p className="mt-2 text-gray-600">introduce tus credenciales para acceder a su centro de trabajo</p>
-        </div>
-        <div className="flex gap-4 mb-8 w-full max-w-md"> 
-          <button className="flex-1 flex items-center justify-center gap-2 h-12 px-4 border border-gray-200 rounded-lg hover:bg-gray-50 transition-colors duration-200">
-            <svg className="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"></path>
-              <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"></path>
-              <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"></path>
-              <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"></path>
-              </svg> google
-          </button>
-          <button className="flex-1 flex items-center justify-center gap-2 h-12 px-4 border border-gray-200 rounded-lg hover:bg-gray-50 transition-colors duration-200">
-           <svg className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.372.823 1.102.823 2.222 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
-          </svg> github
-          </button>
-        </div>
-        <div className="relative mb-8 w-full max-w-md">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-gray-100"></div>
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="px-4 bg-white text-gray-400">o continua con email</span>
-          </div>
-        </div>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          buscarUsuario(credenciales.email, credenciales.password);
-        }} className="w-full max-w-md space-y-6 mb-8">
-          <div>
-            <label htmlFor="email" className="block text-sm font-bold">Dirección de correo</label>
-            <input className="w-full h-14 px-4 bg-background-light border border-gray-100 rounded-lg focus:ring-2 focus:ring-green-600/20 focus:border-green-600 outline-none transition-all duration-200 text-[#101d0c] placeholder:text-gray-400" placeholder="name@company.com" type="email" key="email" value={credenciales.email} onChange={(e) => setCredenciales({...credenciales, email: e.target.value})}>
-            </input>
-          </div>
-          <div className='relative'> {/* //TODO:  cambiar icono */}
-            <div className='flex justify-between'>
-              <label htmlFor="password" className="block text-sm font-bold">Contraseña</label>
-              <a className='text-green-600 font-bold hover:underline' href="">¿olvidaste la Contraseña?</a>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer absolute right-4 top-9 hover:text-green-600" onClick={() => {handleContraseña(!mostrarContraseña)} }>
-              {mostrarContraseña ?
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                  : 
-                <>
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </>
-              }     
-            </svg>
-
-            <input type={mostrarContraseña ? "text" : "password"} key="password" className="w-full h-14 px-4 bg-background-light border border-gray-100 rounded-lg focus:ring-2 focus:ring-green-600/20 focus:border-green-600 outline-none transition-all duration-200 text-[#101d0c] placeholder:text-gray-400" placeholder={mostrarContraseña ? "Contraseña" : "••••••••••"} value={credenciales.password} onChange={(e) => setCredenciales({...credenciales, password: e.target.value})}/>
-          </div>
-          <div>
-           <button className="uppercase w-full h-14 bg-green-600 text-white font-bold rounded-lg shadow-lg shadow-primary/20 hover:bg-green-600/90 transition-all active:scale-[0.98]" type='submit'>
-              entrar
-            </button>
-          </div>
-        </form>
-        <div>
-          <p className="text-gray-500">
-            ¿no tienes una cuenta?
-            <a className="text-green-600 font-bold hover:underline" href="#">registrate gratis</a>
-          </p>
-        </div>
-      </div>
-    </div>
-  )}
+  }
 
 export default App
