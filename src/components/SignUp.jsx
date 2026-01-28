3fe6acdaf447f21f53ca0df8af8841b2e4569e8b
Juan Antonio gonzález Vidal <juanantoniogovidal@gmail.com>
2026-01-28 13:56:41 +0100

    feat: enhance SignUp and InputComponent with icons and improved layout; add LoginButtons for social authentication

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import { InputComponent } from '../components/formsComponents/InputComponent.jsx';
+import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
+import LockClockIcon from '@mui/icons-material/LockClock';
+import EmailIcon from '@mui/icons-material/Email';
+import { LoginButtons } from '../components/formsComponents/LoginButtons.jsx';
 
-export default function SignUp({ isOpen, onClose}) {
+export const SignUp = ({ isOpen, onClose}) => {
     if (!isOpen) return null;
    
 
     return (
-       <div onClick={() => onClose()} className="fixed flex items-center justify-center w-full h-screen bg-gray-900/15 backdrop-blur-sm dark:text-white">
-          <div className='w-full max-w-120 flex flex-col px-15 py-5 bg-blue-950 rounded-lg' onClick={(e) => e.stopPropagation()}>
+       <div onClick={() => onClose()} className="fixed flex items-center justify-center w-full h-screen  backdrop-blur-sm dark:text-white">
+          <div className='w-full max-w-120 flex flex-col px-15 py-5 bg-[#28283d] rounded-3xl' onClick={(e) => e.stopPropagation()}>
             <div className='mb-8'>
                 <h1 className="font-bold text-3xl uppercase dark:text-white">Crea tu cuenta</h1>
                 <p className="mt-2 text-gray-600 dark:text-gray-300">introduce tus credenciales para registrate</p>
             </div>
+            <LoginButtons></LoginButtons>
+        <div className="relative mb-8 w-full max-w-md">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-gray-100 dark:border-gray-700"></div>
+          </div>
+          <div className="relative flex justify-center text-sm">
+            <span className="px-4 bg-white dark:bg-gray-800 text-gray-400 dark:text-gray-300">o continua con email</span>
+          </div>
+        </div>
             <form action="">
                 <div>
-                    <InputComponent label='Nombre completo' placeholder="nombre completo" type="text"/>
+                    <InputComponent Icon={PersonIcon} label='Nombre completo' placeholder="nombre completo" type="text"/>
+                    <InputComponent Icon={EmailIcon} label='Correo Electronico' placeholder="Correo Electronico" type="email"/>
+                    <InputComponent Icon={LockIcon} label='Contraseña' placeholder="Contraseña" type="password"/>
+                    <InputComponent Icon={LockClockIcon} label='Confirmar Contraseña' placeholder="Confirmar Contraseña" type="password"/>
+                    <button className="uppercase w-full h-14 bg-green-600 text-white font-bold rounded-lg shadow-lg shadow-primary/20 hover:bg-green-600/90 transition-all active:scale-[0.98]" type='submit'>
+                        Ingresar
+                    </button>
                 </div>
             </form>
           </div>
        </div>
     );
 }
+
+export default SignUp;
--- src/components/formsComponents/InputComponent.jsx
+++ src/components/formsComponents/InputComponent.jsx
@@ -5,39 +5,57 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 
 
-export const InputComponent = ({ 
-    placeholder = '', 
-    value, 
-    onChange, 
-    type = 'text',
-    label = '',
-    indrec,
+export const InputComponent = ({
+  placeholder = '',
+  value,
+  onChange,
+  type = 'text',
+  label = '',
+  showPasswordRecovery = false,
+  Icon,
 }) => {
 
 
-    const [mostrarContraseña, setmostrarContraseña] = useState(false);
+  const [mostrarContraseña, setmostrarContraseña] = useState(false);
 
-    const typeInput = type === 'password' && mostrarContraseña ? 'text' : type;
+  const [mostrarIcono, setmostrarIcono] = useState(false);
 
-    const placeholderInput = type === 'password' ? (mostrarContraseña ? placeholder : '••••••••••') : placeholder;
+  const typeInput = type === 'password' && mostrarContraseña ? 'text' : type;
 
-    return (
-       <div className='relative'>
-            <div className='flex justify-between'>
-                <label htmlFor="password" className="block text-sm font-bold dark:text-white">{label}</label>
-                    {indrec &&
-                        <a className='hidden sm:flex text-green-600 text-sm font-bold hover:underline' href="">¿olvidaste la Contraseña?</a>
-                    }
-            </div>
-             {type === 'password' && (mostrarContraseña ?
-                <VisibilityOffIcon className="size-6 cursor-pointer absolute right-4 top-9 dark:text-white hover:text-green-600" onClick={() => {setmostrarContraseña(!mostrarContraseña)} }></VisibilityOffIcon>
-                : 
-                <VisibilityIcon className="size-6 cursor-pointer absolute right-4 top-9 dark:text-white hover:text-green-600" onClick={() => {setmostrarContraseña(!mostrarContraseña)} }></VisibilityIcon>
-              )}     
-            <input type={typeInput} className={`${ type === 'password' ? "pr-15" : "" } dark:border-gray-700 dark:bg-gray-700 dark:text-white w-full h-14 px-4 bg-background-light border border-gray-100 rounded-lg focus:ring-2 focus:ring-green-600/20 focus:border-green-600 outline-none transition-all duration-200 text-[#101d0c] placeholder:text-gray-400`} placeholder={placeholderInput} value={value} onChange={onChange}/>
-            {indrec && (
-                <a className='sm:hidden text-green-600 font-bold hover:underline' href="">¿olvidaste la Contraseña?</a>
-            )}
-          </div>
-    );
+  const placeholderInput = type === 'password' ? (mostrarContraseña ? placeholder : '••••••••••') : placeholder;
+
+
+  return (
+    <div className='relative mb-8'>
+      <div className='flex justify-between'>
+        <label htmlFor={label} className="block text-sm font-bold dark:text-white">{label}</label>
+        {showPasswordRecovery &&
+          <a className='hidden sm:flex text-green-600 text-sm font-bold hover:underline' href="">¿olvidaste la Contraseña?</a>
+        }
+      </div>
+      {Icon && (
+        <div>
+          <Icon className="size-6 absolute left-4 top-9 text-black/50 dark:text-white/50" />
+        </div>
+      )}
+
+      {type === 'password' && mostrarIcono && (mostrarContraseña ?
+        <VisibilityOffIcon className="size-6 cursor-pointer absolute right-4 top-9 text-black/50 dark:text-white/50 hover:text-green-600" onMouseDown={(e) => {
+          e.preventDefault();
+          setmostrarContraseña(!mostrarContraseña);
+        }}></VisibilityOffIcon>
+        :
+        <VisibilityIcon className="size-6 cursor-pointer absolute right-4 top-9 text-black/50 dark:text-white/50 hover:text-green-600" onMouseDown={(e) => {
+          e.preventDefault();
+          setmostrarContraseña(!mostrarContraseña);
+        }}></VisibilityIcon>
+      )}
+      <input onBlur={() => setmostrarIcono(false)} onFocus={() => setmostrarIcono(true)} id={label} type={typeInput} className={`${type === 'password' ? "pr-15" : ""} ${Icon ? "pl-13" : ""} dark:border-gray-700 dark:bg-gray-700 dark:text-white w-full h-14 px-4 bg-background-light border border-gray-200 rounded-lg focus:ring-2 focus:ring-green-600/20 focus:border-green-600 outline-none transition-all duration-200 text-[#101d0c] placeholder:text-gray-400`} placeholder={placeholderInput} value={value} onChange={onChange} />
+      {showPasswordRecovery && (
+        <a className='sm:hidden text-green-600 font-bold hover:underline' href="">¿olvidaste la Contraseña?</a>
+      )}
+    </div>
+  );
 };
+
+export default InputComponent;
